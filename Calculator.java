7095f62c283f2c2721386696a42e6b175f09ea78
Sahil-Bhamare <bhamaresahil@gmail.com>
2026-03-26 01:56:44 +0530

    created sub method again

--- Calculator.java
+++ Calculator.java
@@ -3,6 +3,10 @@ public class Calculator {
         return a + b;
     }
 
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+
     public static int multiply(int a, int b) {
         return a * b;
     }
