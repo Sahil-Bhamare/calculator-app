7bcebc475e1becec948e30669f112b208946ad73
Sahil-Bhamare <bhamaresahil@gmail.com>
2026-03-27 23:59:00 +0530

    changed datatype to double

--- Calculator.java
+++ Calculator.java
@@ -1,17 +1,17 @@
 public class Calculator {
-    public static int add(int a, int b) {
+    public static double add(double a, double b) {
         return a + b;
     }
 
-    public static int sub(int a, int b) {
+    public static double sub(double a, double b) {
         return a - b;
     }
 
-    public static int multiply(int a, int b) {
+    public static double multiply(double a, double b) {
         return a * b;
     }
 
-    public static int divide(int a, int b) {
+    public static double divide(double a, double b) {
         return a / b;
     }
 }
