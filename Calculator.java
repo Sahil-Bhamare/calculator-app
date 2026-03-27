f343f3d63aac6590b2dd5603a2190afe872891c9
Sahil-Bhamare <bhamaresahil@gmail.com>
2026-03-28 00:37:37 +0530

    added cli, SOC incoming

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Calculator {
     public static double add(double a, double b) {
         return a + b;
@@ -12,9 +14,51 @@ public class Calculator {
     }
 
     public static double divide(double a, double b) {
-        if(b==0){
+        if (b == 0) {
             System.out.println("Cannot divide by zero");
+            return 0;
         }
         return a / b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("--- CLI CALCULATOR ---");
+            System.out.print("Enter operation :- ( + , - , * , / ) or exit : ");
+            String op = sc.next();
+
+            if(op.equalsIgnoreCase("exit")){
+                System.out.println("CLI Calculator Closed...");
+                break;
+            }
+
+            System.out.println("Enter two numbers:");
+            double a = sc.nextDouble();
+            double b = sc.nextDouble();
+
+            switch (op) {
+                case "+":
+                    System.out.println(Calculator.add(a, b));
+                    break;
+
+                case "-":
+                    System.out.println(Calculator.sub(a, b));
+                    break;
+
+                case "*":
+                    System.out.println(Calculator.multiply(a, b));
+                    break;
+
+                case "/":
+                    System.out.println(Calculator.divide(a, b));
+                    break;
+
+                default:
+                    System.out.println("Invalid operation");
+                    break;
+            }
+        }
     }
 }
