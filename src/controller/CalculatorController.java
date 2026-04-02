5c7545974bdcdcee196a6e4baea8732b5b036139
Sahil-Bhamare <bhamaresahil@gmail.com>
2026-04-02 23:22:14 +0530

    created a controller

--- src/controller/CalculatorController.java
+++ src/controller/CalculatorController.java
@@ -1,5 +1,23 @@
 package src.controller;
 
+import src.service.CalculatorService;
+
 public class CalculatorController {
     
+    private CalculatorService service = new CalculatorService();
+
+    public double calculate(String op, double a, double b) {
+        switch (op) {
+            case "+":
+                return service.add(a, b);
+            case "-":
+                return service.sub(a, b);
+            case "*":
+                return service.multiply(a, b);
+            case "/":
+                return service.divide(a, b);
+            default:
+                throw new IllegalArgumentException("Invalid operation");
+        }
+    }
 }
