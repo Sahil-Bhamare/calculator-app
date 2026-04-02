d12f7d4eda6f98416126e683c69a5e3529fe8f3d
Sahil-Bhamare <bhamaresahil@gmail.com>
2026-04-02 23:24:09 +0530

    separated cli(view) from the logic

--- src/view/CalculatorView.java
+++ src/view/CalculatorView.java
@@ -1,5 +1,40 @@
 package src.view;
 
+import java.util.Scanner;
+
+import src.controller.CalculatorController;
+
 public class CalculatorView {
-    
+    private Scanner sc = new Scanner(System.in);
+    private CalculatorController controller = new CalculatorController();
+
+    public void start() {
+        while (true) {
+            System.out.println("\n-------------------------------");
+            System.out.println("---- SIMPLE CLI CALCULATOR ----");
+            System.out.print("Enter operation (+, -, *, /) or exit: ");
+
+            String op = sc.next();
+
+            if (op.equalsIgnoreCase("exit")) {
+                System.out.println("---- CLI Calculator Closed ----");
+                break;
+            }
+
+            try {
+                System.out.println("Enter two numbers:");
+                double a = sc.nextDouble();
+                double b = sc.nextDouble();
+
+                double result = controller.calculate(op, a, b);
+                System.out.println("Answer: " + result);
+
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+                sc.nextLine(); // clear buffer
+            }
+        }
+        sc.close();
+    }
 }
+
