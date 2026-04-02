8a19e3385373484850e53d3a2c12f2bc9c30f335
Sahil-Bhamare <bhamaresahil@gmail.com>
2026-04-02 23:16:05 +0530

    Moved logic to service layer

--- src/service/CalculatorService.java
+++ src/service/CalculatorService.java
@@ -1,5 +1,22 @@
 package src.service;
 
 public class CalculatorService {
-    
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static double sub(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public static double divide(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is undefined...");
+        }
+        return a / b;
+    }
 }
