1f7a7e011d81bccdebb81663c9157ccfb9a8f9a9
Sahil-Bhamare <bhamaresahil@gmail.com>
2026-04-02 23:38:04 +0530

    resolved a static and instance method mismatch

--- src/service/CalculatorService.java
+++ src/service/CalculatorService.java
@@ -1,19 +1,19 @@
 package src.service;
 
 public class CalculatorService {
-    public static double add(double a, double b) {
+    public double add(double a, double b) {
         return a + b;
     }
 
-    public static double sub(double a, double b) {
+    public double sub(double a, double b) {
         return a - b;
     }
 
-    public static double multiply(double a, double b) {
+    public double multiply(double a, double b) {
         return a * b;
     }
 
-    public static double divide(double a, double b) {
+    public double divide(double a, double b) {
         if (b == 0) {
             throw new ArithmeticException("Division by zero is undefined...");
         }
